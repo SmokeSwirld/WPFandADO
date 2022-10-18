505782979015aedf6c929f660342ba2a1b32b140
sgurn <sgurn@DESKTOP-HP6SAO5>
2022-10-18 18:43:42 +0300

    Add project files.

---
+++ WPFandADO/ADO/ADObasic.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+namespace WPFandADO.ADO
+{
+    /// <summary>
+    /// Interaction logic for ADObasic.xaml
+    /// </summary>
+    public partial class ADObasic : Window
+    {
+        private const String ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\VS file\WPFandADO\WPFandADO\ADO\Database1.mdf"";Integrated Security=True";
+        private SqlConnection connection;  // объект-подключение
+        public ADObasic()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(ConnectionString);
+            // !! ADO создание объекта не открывает подключени
+        }
+
+        private void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connection?.Open();
+                MessageBox.Show("Подключение открыто");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ButtonDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connection?.Close();
+                MessageBox.Show("Подключение закрыто");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ButtonTimestamp_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("SELECT CURRENT_TIMESTAMP", connection))
+            {
+                MessageBox.Show(
+                    cmd.ExecuteScalar()  // исполнение команды и возврат "скаляра" - одного рез-та
+                    .ToString());
+            }
+        }
+    }
+}
---
+++ WPFandADO/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFandADO.ADO;
+using WPFandADO.WPF;
+
+namespace WPFandADO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Containers_Click(object sender, RoutedEventArgs e)
+        {
+            new Containers().ShowDialog();
+        }
+
+        private void ContainersHome_Click(object sender, RoutedEventArgs e)
+        {
+            new ContainersHome().ShowDialog();
+        }
+
+        private void Mine_Click(object sender, RoutedEventArgs e)
+        {
+            new Mine().ShowDialog();
+        }
+
+        private void Canvas_Click(object sender, RoutedEventArgs e)
+        {
+            new Canvasmy().ShowDialog();
+        }
+
+        private void Styles_Click(object sender, RoutedEventArgs e)
+        {
+            new Styles().ShowDialog();
+        }
+
+        private void Triggers_Click(object sender, RoutedEventArgs e)
+        {
+            new Triggers().ShowDialog();
+        }
+
+        private void TriStyHome_Click(object sender, RoutedEventArgs e)
+        {
+            new StylesTriggersHome().ShowDialog();
+        }
+
+        private void DnD_Click(object sender, RoutedEventArgs e)
+        {
+            new DnD().ShowDialog();
+        }
+
+        private void ADObasic_Click(object sender, RoutedEventArgs e)
+        {
+            new ADObasic().ShowDialog();    
+        }
+    }
+}
